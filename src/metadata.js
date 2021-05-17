7f23674d04cc585aacb822f03011adcb1806042b
Jared Parker <parkerjjared@gmail.com>
2021-05-17 19:29:06 +0100

    Build with UserScript Metadata

---
+++ src/metadata.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         KissPlus
+// @namespace    https://github.com/jaredparker/
+// @version      0.0.1
+// @description  Script that adds a custom video player to streaming sites with features such as autoplay.
+// @author       Jared Parker
+// @match        https://kissanime.com.ru/*
+// @icon         https://www.google.com/s2/favicons?domain=tampermonkey.net
+
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_getValue
+// @grant        GM_listValues
+// @grant        GM_addValueChangeListener
+// @grant        GM_removeValueChangeListener
+
+// @grant        GM_openInTab
+// @grant        GM_getTab
+// @grant        GM_getTabs
+// @grant        GM_saveTab
+
+// @grant        GM_addStyle
+// @grant        GM_addElement
+
+// @grant        window.close
+
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+
+// @updateURL    https://github.com/jaredparker/KissPlus/releases/latest/download/kissplus.jaredparker.js
+// @downloadURL  https://github.com/jaredparker/KissPlus/releases/latest/download/kissplus.jaredparker.js
+// @supportURL   https://github.com/jaredparker/KissPlus/issues
+// ==/UserScript==
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,32 @@
 const { merge } = require('webpack-merge');
 const common    = require('./webpack.common.js');
 
+const fs = require('fs');
+
+// FORKED FROM: https://github.com/webpack/webpack-cli/issues/312#issuecomment-732749280
+class BannerPlugin {
+    constructor( options ) {
+        this.banner = options.banner;
+    }
+    apply(compiler) {
+        compiler.hooks.emit.tapAsync( 'FileListPlugin', ( compilation, callback ) => {
+            compilation.chunks.forEach( chunk => {
+                chunk.files.forEach( filename => {
+                    const asset  = compilation.assets[ filename ];
+                    asset._value = `${this.banner}\n\n${asset._value}`; // append banner with line spacing
+                });
+            });
+            callback();
+        });
+    }
+}
+
 module.exports = merge( common, {
 
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new BannerPlugin({
+            banner: fs.readFileSync('./src/metadata.js', 'utf8')
+        })
+    ]
 });
